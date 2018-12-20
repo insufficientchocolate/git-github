f98862c5452f52ed4811a2b66c1ca9e594fe8657
insufficientchocolate <tony84727@gmail.com>
2018-12-20 22:52:47 +0800

    added clang-format target

--- repository.cc
+++ repository.cc
@@ -1,52 +1,48 @@
 #include "repository.hpp"
 
-namespace github
-{
-    Repository::Repository(const char* path): repository_(nullptr, git_repository_free)
-    {
-        git_repository *repo;
-        if(git_repository_init(&repo, path, false)) {
-            const git_error *err = giterr_last();
-            std::string message("failed to init repository: ");
-            message.append(err->message);
-            throw std::runtime_error(message);
-        }
-        repository_ = std::unique_ptr<git_repository, decltype(&git_repository_free)>(repo, git_repository_free);
-    }
-    
-    std::string Repository::getHeadOID()
-    {
-        git_annotated_commit *commit;
-        if(git_annotated_commit_from_ref(&commit, repository_.get(), getHead().get()))
-        {
-            throw std::runtime_error("failed to get annotated commit");
-        }
-        const git_oid *oid = git_annotated_commit_id(commit);
-        git_annotated_commit_free(commit);
-        
-        return std::string(reinterpret_cast<const char*>(oid->id));
-    }
-    
-    std::shared_ptr<git_reference> Repository::getHead()
-    {
-        git_reference *out;
-        if(git_repository_head(&out, repository_.get())) {
-            const git_error *err = giterr_last();
-            std::string message("failed to read repository head: ");
-            message.append(err->message);
-            throw std::runtime_error(message);
-        }
-        return std::shared_ptr<git_reference>(out, git_reference_free);
-    }
-    
-    void Repository::throwGitError(std::string prefix)
-    {
-        const git_error *err = giterr_last();
-        if (!err)
-        {
-            return;
-        }
-        prefix.append(err->message);
-        throw std::runtime_error(prefix);
-    }
-};
+namespace github {
+Repository::Repository(const char *path)
+    : repository_(nullptr, git_repository_free) {
+  git_repository *repo;
+  if (git_repository_init(&repo, path, false)) {
+    const git_error *err = giterr_last();
+    std::string message("failed to init repository: ");
+    message.append(err->message);
+    throw std::runtime_error(message);
+  }
+  repository_ = std::unique_ptr<git_repository, decltype(&git_repository_free)>(
+      repo, git_repository_free);
+}
+
+std::string Repository::getHeadOID() {
+  git_annotated_commit *commit;
+  if (git_annotated_commit_from_ref(&commit, repository_.get(),
+                                    getHead().get())) {
+    throw std::runtime_error("failed to get annotated commit");
+  }
+  const git_oid *oid = git_annotated_commit_id(commit);
+  git_annotated_commit_free(commit);
+
+  return std::string(reinterpret_cast<const char *>(oid->id));
+}
+
+std::shared_ptr<git_reference> Repository::getHead() {
+  git_reference *out;
+  if (git_repository_head(&out, repository_.get())) {
+    const git_error *err = giterr_last();
+    std::string message("failed to read repository head: ");
+    message.append(err->message);
+    throw std::runtime_error(message);
+  }
+  return std::shared_ptr<git_reference>(out, git_reference_free);
+}
+
+void Repository::throwGitError(std::string prefix) {
+  const git_error *err = giterr_last();
+  if (!err) {
+    return;
+  }
+  prefix.append(err->message);
+  throw std::runtime_error(prefix);
+}
+};  // namespace github
--- repository.hpp
+++ repository.hpp
@@ -1,25 +1,24 @@
 #ifndef _GIT_GITHUB_REPOSITORY_H_
 #define _GIT_GITHUB_REPOSITORY_H_
-#include <git2/repository.h>
 #include <git2/annotated_commit.h>
-#include <git2/refs.h>
-#include <git2/global.h>
 #include <git2/errors.h>
-#include <memory>
+#include <git2/global.h>
+#include <git2/refs.h>
+#include <git2/repository.h>
 #include <array>
+#include <memory>
 #include <string>
-namespace github
-{
-    class Repository
-    {
-    public:
-        Repository(const char* path);
-        std::shared_ptr<git_reference> getHead();
-        std::string getHeadOID();
-        static void throwGitError(std::string prefixMessage = "");
-    private:
-       std::unique_ptr<git_repository, decltype(&git_repository_free)> repository_;
-    };
+namespace github {
+class Repository {
+ public:
+  Repository(const char* path);
+  std::shared_ptr<git_reference> getHead();
+  std::string getHeadOID();
+  static void throwGitError(std::string prefixMessage = "");
+
+ private:
+  std::unique_ptr<git_repository, decltype(&git_repository_free)> repository_;
 };
+};  // namespace github
 
 #endif
