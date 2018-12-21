e2b6298e1060aeb51000d28332df72c039b05cc4
insufficientchocolate <tony84727@gmail.com>
2018-12-22 00:30:01 +0800

    implement repository discovery & subcommand

--- main.cc
+++ main.cc
@@ -1,10 +1,38 @@
+#include <git2/buffer.h>
 #include <git2/global.h>
 #include <unistd.h>
+#include <CLI/CLI.hpp>
 #include <iostream>
 #include "repository.hpp"
 
-int main(int argc, char** args) {
-  github::Repository repo(".git");
-  std::cout << "Current head is " << repo.getHeadSHA() << std::endl;
+const std::string findRepository() {
+  git_buf out = {0};
+  git_repository *repo;
+
+  char *wd = getwd(NULL);
+
+  if (git_repository_discover(&out, wd, true, nullptr)) {
+    github::Repository::throwGitError();
+  }
+  free(wd);
+  const std::string path(out.ptr);
+  return path;
+}
+
+int main(int argc, char **argv) {
+  if (git_libgit2_init()) {
+    github::Repository::throwGitError();
+  }
+  const std::string repositoryPath = findRepository();
+  CLI::App app("git extension for github", "git-github");
+  app.add_subcommand("download")->callback([repositoryPath] {
+    github::Repository repo(repositoryPath.c_str());
+    std::string head = repo.getHeadSHA();
+    std::cout << "Current head is " << head << std::endl;
+    std::cout << "Donwloading " << head << std::endl;
+  });
+
+  CLI11_PARSE(app, argc, argv);
+
   return 0;
 }
